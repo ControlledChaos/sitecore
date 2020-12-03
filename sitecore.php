abdeeb2fc13864d16c22c6b5dfc8a95da7733615
ControlledChaos <greg@ccdzine.com>
2020-12-02 17:36:57 -0800

    Define global constants.

--- sitecore.php
+++ sitecore.php
@@ -80,3 +80,81 @@
  */
 
 namespace SiteCore;
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+/**
+ * Constant: Required PHP version
+ *
+ * @since  1.0.0
+ * @return string Returns the minimum required PHP version.
+ */
+if ( ! defined( 'SCP_PHP_VERSION' ) ) {
+	define( 'SCP_PHP_VERSION', '7.3' );
+}
+
+/**
+ * Constant: Plugin version
+ *
+ * Keeping the version at 1.0.0 as this is a starter plugin but
+ * you may want to start counting as you develop for your use case.
+ *
+ * @since  1.0.0
+ * @return string Returns the latest plugin version.
+ */
+if ( ! defined( 'SCP_VERSION' ) ) {
+	define( 'SCP_VERSION', '1.0.0' );
+}
+
+/**
+ * Constant: Text domain
+ *
+ * @since  1.0.0
+ * @return string Returns the text domain of the plugin.
+ *
+ * @todo   Replace all strings with constant.
+ */
+if ( ! defined( 'SCP_DOMAIN' ) ) {
+	define( 'SCP_DOMAIN', 'sitecore' );
+}
+
+/**
+ * Constant: Plugin folder path
+ *
+ * @since  1.0.0
+ * @return string Returns the filesystem directory path (with trailing slash)
+ *                for the plugin __FILE__ passed in.
+ */
+if ( ! defined( 'SCP_PATH' ) ) {
+	define( 'SCP_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+/**
+ * Constant: Plugin folder URL
+ *
+ * @since  1.0.0
+ * @return string Returns the URL directory path (with trailing slash)
+ *                for the plugin __FILE__ passed in.
+ */
+if ( ! defined( 'SCP_URL' ) ) {
+	define( 'SCP_URL', plugin_dir_url( __FILE__ ) );
+}
+
+/**
+ * Constant: Universal slug
+ *
+ * This URL slug is used for various plugin admin & settings pages.
+ *
+ * The prefix will change in your search & replace in renaming the plugin.
+ * Change the second part of the define(), here as 'sitecore',
+ * to your preferred page slug.
+ *
+ * @since  1.0.0
+ * @return string Returns the URL slug of the admin pages.
+ */
+if ( ! defined( 'SCP_ADMIN_SLUG' ) ) {
+	define( 'SCP_ADMIN_SLUG', 'sitecore' );
+}
