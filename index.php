776024738f0b92bf05d5b49d687fd106c99226b8
ControlledChaos <greg@ccdzine.com>
2020-12-03 12:43:23 -0800

    Main index file comment/docblock.

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php // Security file.
+<?php
+/**
+ * Site Core Plugin
+ *
+ * Develop site-specific plugins for ClassicPress, WordPress, and the antibrand system.
+ *
+ * @package Site_Core
+ * @version 1.0.0
+ * @author  Greg Sweet <greg@ccdzine.com>
+ * @link    https://github.com/ControlledChaos/sitecore
+ *
+ * This file is provided for security. In keeping with tradition
+ * the main plugin file with the necessary plugin header is in
+ * the root directory, with this file, and is of the same name as
+ * the plugin folder. However, this file can be deleted and the
+ * main plugin file be renamed as `index.php`.
+ */
