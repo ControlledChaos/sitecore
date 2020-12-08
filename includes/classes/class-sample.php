880252bd24c11202553904b8e0b8ab2dac583099
ControlledChaos <greg@ccdzine.com>
2020-12-08 08:39:20 -0800

    Sample/starter class file.

---
+++ includes/classes/class-sample.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Sample/starter class
+ *
+ * @package    Site_Core
+ * @subpackage Includes
+ * @category   Classes
+ * @access     public
+ * @since      1.0.0
+ */
+
+namespace SiteCore\Classes;
+
+// Restrict direct access.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+class Sample {
+
+	/**
+	 * Constructor method
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return self
+	 */
+	public function __construct() {}
+}
