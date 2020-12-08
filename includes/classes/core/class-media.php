3fd75b400ec2741ddc38b8f13d7d3c0d5d4eb13a
ControlledChaos <greg@ccdzine.com>
2020-12-08 08:33:22 -0800

    New media class file.

---
+++ includes/classes/core/class-media.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Media class
+ *
+ * @package    Site_Core
+ * @subpackage Classes
+ * @category   Includes
+ * @access     public
+ * @since      1.0.0
+ */
+
+namespace SiteCore\Classes\Core;
+
+// Restrict direct access.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+class Media {
+
+	/**
+	 * Constructor method
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return self
+	 */
+	public function __construct() {}
+}
