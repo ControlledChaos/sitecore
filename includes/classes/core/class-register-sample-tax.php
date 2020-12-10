893259b874fbfca895dd26967f38f98d01a189ad
ControlledChaos <greg@ccdzine.com>
2020-12-10 13:06:31 -0800

    Restore sample taxonomy.

---
+++ includes/classes/core/class-register-sample-tax.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Register sample taxonomy
+ *
+ * @package    Site_Core
+ * @subpackage Classes
+ * @category   Core
+ * @access     public
+ * @since      1.0.0
+ */
+
+declare( strict_types = 1 );
+namespace SiteCore\Classes\Core;
+
+// Restrict direct access.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+class Register_Sample_Tax extends Register_Tax {
+
+	/**
+	 * Taxonomy
+	 *
+	 * Maximum 20 characters. May only contain lowercase alphanumeric
+	 * characters, dashes, and underscores. Dashes discouraged.
+	 *
+	 * @example 'color'
+	 * @example 'vehicle_type'
+	 *
+	 * @since  1.0.0
+	 * @access protected
+	 * @var    string The database name of the taxonomy.
+	 */
+	protected $tax_key = 'sample_tax';
+
+	/**
+	 * Associated post types
+	 *
+	 * @since  1.0.0
+	 * @access protected
+	 * @var    array The array of associated post types.
+	 */
+	protected $post_types = [
+		'post'
+	];
+
+	/**
+	 * Singular name
+	 *
+	 * @since  1.0.0
+	 * @access protected
+	 * @var    string The singular name of the taxonomy.
+	 */
+	protected $singular = 'sample tax';
+
+	/**
+	 * Plural name
+	 *
+	 * @since  1.0.0
+	 * @access protected
+	 * @var    string The plural name of the taxonomy.
+	 */
+	protected $plural = 'sample taxes';
+
+	/**
+	 * Constructor magic method.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return self
+	 */
+	public function __construct() {
+
+		// Run the parent constructor method.
+		parent :: __construct();
+
+	}
+}
