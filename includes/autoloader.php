a0fde761ad1a458c71d87a18d4ab0463d03f7f67
ControlledChaos <greg@ccdzine.com>
2020-12-11 12:36:03 -0800

    Autoloader: define arrays with constants.

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -18,14 +18,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
-// Define the class directories and file prefixes.
-define( 'SCP_CLASS_PATH',    SCP_PATH . 'includes/classes/' );
-define( 'SCP_CORE_CLASS',    SCP_CLASS_PATH . 'core/class-' );
-define( 'SCP_MEDIA_CLASS',   SCP_CLASS_PATH . 'media/class-' );
-define( 'SCP_VENDOR_CLASS',  SCP_CLASS_PATH . 'vendor/class-' );
-define( 'SCP_ADMIN_CLASS',   SCP_CLASS_PATH . 'backend/class-' );
-define( 'SCP_FRONT_CLASS',   SCP_CLASS_PATH . 'frontend/class-' );
-define( 'SCP_GENERAL_CLASS', SCP_CLASS_PATH . 'class-' );
+/**
+ * Class files
+ *
+ * Defines the class directories and file prefixes.
+ *
+ * @since 1.0.0
+ * @var   array Defines an array of class file paths.
+ */
+define( 'SCP_CLASS', [
+	'core'    => SCP_PATH . 'includes/classes/core/class-',
+	'media'   => SCP_PATH . 'includes/classes/media/class-',
+	'vendor'  => SCP_PATH . 'includes/classes/vendor/class-',
+	'admin'   => SCP_PATH . 'includes/classes/backend/class-',
+	'front'   => SCP_PATH . 'includes/classes/frontend/class-',
+	'general' => SCP_PATH . 'includes/classes/class-',
+] );
 
 /**
  * Array of classes to register
@@ -33,37 +41,45 @@ define( 'SCP_GENERAL_CLASS', SCP_CLASS_PATH . 'class-' );
  * When you add new classes to your version of this plugin you may
  * add them to the following array rather than requiring the file
  * elsewhere. Be sure to include the precise namespace.
+ *
+ * @since 1.0.0
+ * @var   array Defines an array of class files to register.
  */
-const SCP_CLASSES = [
+define( 'SCP_CLASSES', [
 
 	// Core classes.
-	'SiteCore\Classes\Core\Editor_Options'      => SCP_CORE_CLASS . 'editor-options.php',
-	'SiteCore\Classes\Core\Type_Tax'            => SCP_CORE_CLASS . 'type-tax.php',
-	'SiteCore\Classes\Core\Register_Type'       => SCP_CORE_CLASS . 'register-type.php',
-	'SiteCore\Classes\Core\Register_Sample_Tax' => SCP_CORE_CLASS . 'register-sample-tax.php',
-	'SiteCore\Classes\Core\Register_Tax'        => SCP_CORE_CLASS . 'register-tax.php',
-	'SiteCore\Classes\Core\Register_Media_Type' => SCP_CORE_CLASS . 'register-media-type.php',
+	'SiteCore\Classes\Core\Editor_Options'      => SCP_CLASS['core'] . 'editor-options.php',
+	'SiteCore\Classes\Core\Type_Tax'            => SCP_CLASS['core'] . 'type-tax.php',
+	'SiteCore\Classes\Core\Register_Type'       => SCP_CLASS['core'] . 'register-type.php',
+	'SiteCore\Classes\Core\Register_Tax'        => SCP_CLASS['core'] . 'register-tax.php',
+	'SiteCore\Classes\Core\Register_Media_Type' => SCP_CLASS['core'] . 'register-media-type.php',
 
 	// Media classes.
-	'SiteCore\Classes\Media\Media' => SCP_MEDIA_CLASS . 'media.php',
+	'SiteCore\Classes\Media\Media' => SCP_CLASS['media'] . 'media.php',
 
 	// Vendor classes.
-	'SiteCore\Classes\Vendor\Plugins' => SCP_VENDOR_CLASS . 'plugins.php',
-	'SiteCore\Classes\Vendor\ACF'     => SCP_VENDOR_CLASS . 'acf.php',
+	'SiteCore\Classes\Vendor\Plugins' => SCP_CLASS['vendor'] . 'plugins.php',
+	'SiteCore\Classes\Vendor\ACF'     => SCP_CLASS['vendor'] . 'acf.php',
 
 	// Backend/admin classes,
-	'SiteCore\Classes\Admin\Admin'        => SCP_ADMIN_CLASS . 'admin.php',
-	'SiteCore\Classes\Admin\Admin_Screen' => SCP_ADMIN_CLASS . 'admin-screen.php',
-	'SiteCore\Classes\Admin\Dashboard'    => SCP_ADMIN_CLASS . 'dashboard.php',
+	'SiteCore\Classes\Admin\Admin'        => SCP_CLASS['admin'] . 'admin.php',
+	'SiteCore\Classes\Admin\Admin_Screen' => SCP_CLASS['admin'] . 'admin-screen.php',
+	'SiteCore\Classes\Admin\Dashboard'    => SCP_CLASS['admin'] . 'dashboard.php',
 
 	// Frontend classes.
-	'SiteCore\Classes\Front\Frontend' => SCP_FRONT_CLASS . 'frontend.php',
+	'SiteCore\Classes\Front\Frontend' => SCP_CLASS['front'] . 'frontend.php',
 
 	// General/miscellaneos classes.
-	'SiteCore\Classes\User_Toolbar' => SCP_GENERAL_CLASS . 'user-toolbar.php'
-];
+	'SiteCore\Classes\User_Toolbar' => SCP_CLASS['general'] . 'user-toolbar.php'
+] );
 
-// Autoload class files.
+/**
+ * Autoload class files
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
 spl_autoload_register(
 	function ( string $classname ) {
 		if ( isset( SCP_CLASSES[ $classname ] ) ) {
