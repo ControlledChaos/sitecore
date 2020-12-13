24c0ecde0102e9f36295a2d821e0f0a6197f6ff3
ControlledChaos <greg@ccdzine.com>
2020-12-12 19:09:54 -0800

    Backend and frontend features added.
    User toolbar stuff, hide the & plugin editor pages, admin footer...

--- includes/classes/backend/class-admin.php
+++ includes/classes/backend/class-admin.php
@@ -28,16 +28,110 @@ class Admin {
 	 */
 	public function __construct() {
 
-		// Get the filename of the current page.
+		// Remove theme & plugin editor links.
+		add_action( 'admin_init', [ $this, 'remove_editor_links' ] );
+
+		// Redirect theme & plugin editor pages.
+		add_action( 'admin_init', [ $this, 'redirect_editor_pages' ] );
+
+		// Remove the ClassicPress/WordPress logo from the admin bar.
+		add_action( 'admin_bar_menu', [ $this, 'remove_toolbar_logo' ], 999 );
+
+		// Hide the ClassicPress/WordPress update notification to all but admins.
+		add_action( 'admin_head', [ $this, 'admin_only_updates' ], 1 );
+
+		// Credits in admin footer.
+		add_filter( 'admin_footer_text', [ $this, 'admin_footer' ], 1 );
+	}
+
+	/**
+	 * Remove theme & plugin editor links.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return array
+	 */
+	public function remove_editor_links() {
+
+		$remove_theme_editor  = remove_submenu_page( 'themes.php', 'theme-editor.php' );
+		$remove_plugin_editor = remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+
+		return [ $remove_theme_editor, $remove_plugin_editor ];
+	}
+
+	/**
+	 * Redirect theme & plugin editor pages
+	 *
+	 * A temporary redirect to the dashboard is created.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @global object pagenow Gets the current admin screen.
+	 * @return void
+	 */
+	public function redirect_editor_pages() {
+
 		global $pagenow;
 
-		// Run related classes.
-		new Add_Menu_Page;
-		new Add_Submenu_Page;
-
-		// Run the dashboard only on the backend index screen.
-		if ( 'index.php' == $pagenow ) {
-			new Dashboard;
+		// Redirect if user is on the theme or plugin editor page.
+		if ( $pagenow == 'plugin-editor.php' || $pagenow == 'theme-editor.php' ) {
+			wp_redirect( admin_url( '/', 'http' ), 302 );
+			exit;
 		}
+	}
+
+	/**
+	 * Remove the ClassicPress/WordPress logo from the admin bar.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  object $wp_admin_bar
+	 * @return void
+	 *
+	 * @todo Make this optional on the Site Settings screen.
+	 */
+	public function remove_toolbar_logo( $wp_admin_bar ) {
+
+		$wp_admin_bar->remove_node( 'wp-logo' );
+	}
+
+	/**
+	 * Hide the ClassicPress/WordPress update notification to all but admins.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 *
+	 * @todo Make this optional on the Site Settings screen.
+	 */
+	public function admin_only_updates() {
+
+		// The `update_core` capability includes admins and super admins.
+		if ( ! current_user_can( 'update_core' ) ) {
+			remove_action( 'admin_notices', 'update_nag', 3 );
+		}
+	}
+
+	/**
+	 * Admin footer
+	 *
+	 * Replaces the "Thank you for creating with ClassicPress/WordPress" text
+	 * in the #wpfooter div at the bottom of all admin screens.
+	 *
+	 * The output strings contain a trailing space after the period
+	 * because other plugins may also tap into the footer. a high
+	 * priority is used on the hook in attempt to put our text first.
+	 *
+	 * This replaces text inside the default paragraph (<p>) tags.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return string
+	 */
+	public function admin_footer() {
+
+		$content = get_bloginfo( 'name' );
+
+		echo $content;
 	}
 }
--- includes/classes/frontend/class-frontend.php
+++ includes/classes/frontend/class-frontend.php
@@ -25,5 +25,44 @@ class Frontend {
 	 * @access public
 	 * @return self
 	 */
-	public function __construct() {}
+	public function __construct() {
+
+		// Remove the ClassicPress/WordPress logo from the admin bar.
+		add_action( 'admin_bar_menu', [ $this, 'remove_toolbar_logo' ], 999 );
+
+		// Remove search from frontend admin toolbar.
+		add_action( 'wp_before_admin_bar_render', [ $this, 'remove_toolbar_search' ] );
+	}
+
+	/**
+	 * Remove the ClassicPress/WordPress logo from the admin bar.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  object $wp_admin_bar
+	 * @return void
+	 *
+	 * @todo Make this optional.
+	 */
+	public function remove_toolbar_logo( $wp_admin_bar ) {
+
+		$wp_admin_bar->remove_node( 'wp-logo' );
+	}
+
+	/**
+	 * Remove the search bar from the frontend admin toolbar.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @global object wp_admin_bar
+	 * @return void
+	 *
+	 * @todo Make this optional.
+	 */
+	public function remove_toolbar_search() {
+
+		global $wp_admin_bar;
+
+		$wp_admin_bar->remove_menu( 'search' );
+	}
 }
