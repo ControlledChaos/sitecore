1e7675ffecdd2d83dadb33636e718540773d63f9
ControlledChaos <greg@ccdzine.com>
2020-12-12 20:15:44 -0800

    Add features to the frontend class.

--- includes/classes/frontend/class-frontend.php
+++ includes/classes/frontend/class-frontend.php
@@ -27,11 +27,76 @@ class Frontend {
 	 */
 	public function __construct() {
 
+		// Remove unpopular meta tags.
+		add_action( 'init', [ $this, 'head_cleanup' ] );
+
+		// Remove system versions from stylesheets and scripts.
+		add_filter( 'style_loader_src', [ $this, 'remove_versions' ], 999 );
+		add_filter( 'script_loader_src', [ $this, 'remove_versions' ], 999 );
+
+		// Disable emoji script.
+		add_action( 'init', [ $this, 'disable_emojis' ] );
+
 		// Remove the ClassicPress/WordPress logo from the admin bar.
 		add_action( 'admin_bar_menu', [ $this, 'remove_toolbar_logo' ], 999 );
 
 		// Remove search from frontend admin toolbar.
 		add_action( 'wp_before_admin_bar_render', [ $this, 'remove_toolbar_search' ] );
+	}
+
+	/**
+	 * Clean up meta tags from the <head>
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function head_cleanup() {
+
+		remove_action( 'wp_head', 'rsd_link' );
+		remove_action( 'wp_head', 'wlwmanifest_link' );
+		remove_action( 'wp_head', 'wp_generator' );
+	}
+
+	/**
+	 * Remove system versions
+	 *
+	 * Removes the system versions from stylesheet and script inks
+	 * in the head. The versions are a potential security risk,
+	 * indicating which version of the system to attack, and force
+	 * browsers to download new scripts when the system updates.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string $src Path to the file.
+	 * @return null
+	 */
+	public function remove_versions( $src ) {
+
+		if ( strpos( $src, '?ver=' ) ) {
+			$src = remove_query_arg( 'ver', $src );
+		}
+		return $src;
+	}
+
+	/**
+	 * Disable emoji script
+	 *
+	 * Emojis will still work in modern browsers. This removes the script
+	 * that makes emojis work in old browser.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function disable_emojis() {
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	}
 
 	/**
@@ -45,7 +110,6 @@ class Frontend {
 	 * @todo Make this optional.
 	 */
 	public function remove_toolbar_logo( $wp_admin_bar ) {
-
 		$wp_admin_bar->remove_node( 'wp-logo' );
 	}
 
