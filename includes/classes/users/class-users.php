a0fb6f7b0b7dd873d9d9f4fb360909611651d1bc
ControlledChaos <greg@ccdzine.com>
2020-12-15 13:53:00 -0800

    Move Personal Data pages under Users in admin menu.

--- includes/classes/users/class-users.php
+++ includes/classes/users/class-users.php
@@ -25,5 +25,57 @@ class Users {
 	 * @access public
 	 * @return self
 	 */
-	public function __construct() {}
+	public function __construct() {
+
+		// Move the personal data menu items.
+        add_action( 'admin_menu', [ $this, 'menus_personal_data' ] );
+	}
+
+	/**
+	 * Move the personal data
+	 *
+	 * Moves the personal data links to the Users entry.
+	 *
+	 * @since  1.0.0
+	 * @access public
+     * @global array $menu The admin menu array.
+     * @global array $submenu The admin submenu array.
+	 * @return void
+	 */
+	public function menus_personal_data() {
+
+		global $menu, $submenu;
+
+		// Remove personal data links as submenu items of Tools.
+		if ( isset( $submenu['tools.php'] ) ) {
+
+			// Look for menu items under Tools.
+			foreach ( $submenu['tools.php'] as $key => $item ) {
+
+				// Unset Export if it is found.
+				if ( $item[2] === 'export-personal-data.php' ) {
+					unset($submenu['tools.php'][$key] );
+				}
+
+				// Unset Erase if it is found.
+				if ( $item[2] === 'erase-personal-data.php' ) {
+					unset( $submenu['tools.php'][$key] );
+				}
+
+			}
+		}
+
+		// Personal data page files.
+		$submenu['users.php'][25] = [
+			__( 'Export Personal Data', SCP_DOMAIN ),
+			'export_others_personal_data',
+			'export-personal-data.php'
+		];
+
+		$submenu['users.php'][30] = [
+			__( 'Erase Personal Data', SCP_DOMAIN ),
+			'erase_others_personal_data',
+			'erase-personal-data.php'
+		];
+	}
 }
