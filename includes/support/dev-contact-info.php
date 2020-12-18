5f9342e72fc0dbaed5a7189f5eb50d3af857b416
ControlledChaos <greg@ccdzine.com>
2020-12-17 23:24:00 -0800

    Add constants to dev contact/info.

--- includes/support/dev-contact-info.php
+++ includes/support/dev-contact-info.php
@@ -2,8 +2,44 @@
 /**
  * Developer and contact information
  *
+ * For display of developer credits and contact information.
+ *
  * @package    Site_Core
  * @subpackage Includes
  * @category   Support
  * @since      1.0.0
  */
+
+namespace SiteCore;
+
+/**
+ * Constant: Developer name
+ *
+ * @since 1.0.0
+ * @var   string The name of the developer/agency.
+ */
+define( 'SCP_DEV_NAME', 'Controlled Chaos' );
+
+/**
+ * Constant: Developer URL
+ *
+ * @since 1.0.0
+ * @var   string The URL of the developer/agency.
+ */
+define( 'SCP_DEV_URL', 'https://ccdzine.com/' );
+
+/**
+ * Constant: Developer email
+ *
+ * @since 1.0.0
+ * @var   string The URL of the developer/agency.
+ */
+define( 'SCP_DEV_EMAIL', 'greg@ccdzine.com' );
+
+/**
+ * Constant: Plugin URL
+ *
+ * @since 1.0.0
+ * @var   string The URL of the plugin.
+ */
+define( 'SCP_PLUGIN_URL', 'https://github.com/ControlledChaos/sitecore' );
