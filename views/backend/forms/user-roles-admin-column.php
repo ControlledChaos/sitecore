d5930b1410686e67afa5e508223b459957831db6
ControlledChaos <greg@ccdzine.com>
2020-12-19 22:16:14 -0800

    Add multiple roles per user.

---
+++ views/backend/forms/user-roles-admin-column.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User roles admin column
+ *
+ * Outputs a list of roles belonging to the current user.
+ *
+ * @package    Site_Core
+ * @subpackage Views
+ * @category   Forms
+ * @since      1.0.0
+ *
+ * @var $roles array All applicable roles in name => label pairs.
+ */
+?><div class="md-multiple-roles">
+	<?php if ( ! empty( $roles ) ) :
+		foreach( $roles as $name => $label ) :
+			$roles[$name] = '<a href="users.php?role=' . esc_attr( $name ) . '">' . esc_html( translate_user_role( $label ) ) . '</a>';
+		endforeach;
+		echo implode( ', ', $roles );
+	else : ?>
+		<span class="md-multiple-roles-no-role"><?php _e( 'None', 'multiple-roles' ); ?></span>
+	<?php endif; ?>
+</div><!-- .md-multiple-roles -->
---
+++ views/backend/forms/user-roles-checklist.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Output the user roles checklist
+ *
+ * @package    Site_Core
+ * @subpackage Views
+ * @category   Forms
+ * @since      1.0.0
+ *
+ * @var $roles array All WordPress roles in name => label pairs.
+ * @var $user_roles array An array of role names belonging to the current user.
+ */
+// $creating       = isset( $_POST['createuser'] );
+// $selected_roles = $creating && isset( $_POST['md_multiple_roles'] ) ? wp_unslash( $_POST['md_multiple_roles'] ) : '';
+
+if ( isset( $_POST['createuser'] ) && isset( $_POST['md_multiple_roles'] ) ) {
+	$selected_roles = wp_unslash( $_POST['md_multiple_roles'] );
+} else {
+	$selected_roles = '';
+}
+
+?>
+<h3><?php _e( 'Permissions', SCP_DOMAIN ); ?></h3>
+
+<table class="form-table">
+	<tr>
+		<th><?php _e( 'Roles', SCP_DOMAIN ); ?></th>
+		<td>
+			<?php foreach( $roles as $name => $label ) :
+				$input_uniq_id = uniqid(); ?>
+				<label for="md-multiple-roles-<?php echo esc_attr( $name ) . '-' . $input_uniq_id; ?>">
+					<input
+						id="md-multiple-roles-<?php echo esc_attr( $name ) . '-' . $input_uniq_id; ?>"
+						type="checkbox"
+						name="md_multiple_roles[]"
+						value="<?php echo esc_attr( $name ); ?>"
+						<?php
+						// Edit user page.
+						if ( ! is_null( $user_roles ) ) {
+							checked( in_array( $name, $user_roles ) );
+
+						// Add new user page.
+						} elseif ( ! empty( $selected_roles ) ) {
+							checked( in_array( $name, $selected_roles ) );
+						}
+						?>
+					/>
+					<?php echo esc_html( translate_user_role( $label ) ); ?>
+				</label>
+				<br />
+			<?php endforeach; ?>
+		</td>
+	</tr>
+</table>
