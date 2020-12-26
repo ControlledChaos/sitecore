b2c1506193d32eeecf14b47a0bdb84db8e297d3e
ControlledChaos <greg@ccdzine.com>
2020-12-26 12:53:40 -0800

    Settings class: remove sample properties.

--- includes/classes/settings/class-settings.php
+++ includes/classes/settings/class-settings.php
@@ -26,52 +26,6 @@ define( 'SCP_FORMS', [
 class Settings extends Classes\Base {
 
 	/**
-	 * Sample string
-	 *
-	 * Document how and where this is used.
-	 *
-	 * @since  1.0.0
-	 * @access protected
-	 * @var    string Returns the string.
-	 *                Document what is expected or required.
-	 */
-	protected $sample_string = 'Sample string variable';
-
-	/**
-	 * Sample integer
-	 *
-	 * Document how and where this is used.
-	 *
-	 * @since  1.0.0
-	 * @access protected
-	 * @var    integer Returns the integer.
-	 *                Document what is expected or required.
-	 */
-	protected $sample_integer = 33;
-
-	/**
-	 * Sample array
-	 *
-	 * Document how and where this is used.
-	 *
-	 * @since  1.0.0
-	 * @access protected
-	 * @var    array Returns the array.
-	 */
-	protected $sample_array = [];
-
-	/**
-	 * Sample boolean
-	 *
-	 * Document how and where this is used.
-	 *
-	 * @since  1.0.0
-	 * @access protected
-	 * @var    boolean Returns true or false.
-	 */
-	protected $sample_boolean = false;
-
-	/**
 	 * Constructor method
 	 *
 	 * Calls the parent constructor.
