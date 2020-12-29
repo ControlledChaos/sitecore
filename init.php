956a79fe0c64a74bc1cdffda7bc725a2cf532fb0
ControlledChaos <greg@ccdzine.com>
2020-12-29 08:16:23 -0800

    Move pluggable check to compatibility file.

--- init.php
+++ init.php
@@ -40,39 +40,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function sitecore() {
 
-	// Get the filename of the current page.
-	global $pagenow;
-
-	/**
-	 * Get plugins path
-	 *
-	 * Used to check for active plugins with the `is_plugin_active` function.
-	 */
-
-	// Compatibility with ClassicPress and WordPress.
-	if ( file_exists( ABSPATH . 'wp-admin/includes/plugin.php' ) ) {
-		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-
-	// Compatibility with the antibrand system.
-	} elseif ( defined( 'APP_INC_PATH' ) && file_exists( APP_INC_PATH . '/backend/plugin.php' ) ) {
-		include_once( APP_INC_PATH . '/backend/plugin.php' );
-	}
-
-	/**
-	 * Get pluggable path
-	 *
-	 * Used to check for the `is_user_logged_in` function.
-	 */
-
-	// Compatibility with ClassicPress and WordPress.
-	if ( ( function_exists( 'is_multisite' ) && ! is_multisite() ) && file_exists( ABSPATH . 'wp-includes/pluggable.php' ) ) {
-		include_once( ABSPATH . 'wp-includes/pluggable.php' );
-
-	// Compatibility with the antibrand system.
-	} elseif ( ( function_exists( 'is_network' ) && ! is_network() ) && defined( 'APP_INC_PATH' ) && file_exists( APP_INC_PATH . '/pluggable.php' ) ) {
-		include_once( APP_INC_PATH . '/pluggable.php' );
-	}
-
 	/**
 	 * Class autoloader
 	 *
--- vendor/compatibility.php
+++ vendor/compatibility.php
@@ -11,6 +11,21 @@
 namespace SiteCore;
 
 /**
+ * Get pluggable path
+ *
+ * Used to check for the `is_user_logged_in` function.
+ */
+
+// Compatibility with ClassicPress and WordPress.
+if ( ( function_exists( 'is_multisite' ) && ! is_multisite() ) && file_exists( ABSPATH . 'wp-includes/pluggable.php' ) ) {
+	include_once( ABSPATH . 'wp-includes/pluggable.php' );
+
+// Compatibility with the antibrand system.
+} elseif ( ( function_exists( 'is_network' ) && ! is_network() ) && defined( 'APP_INC_PATH' ) && file_exists( APP_INC_PATH . '/pluggable.php' ) ) {
+	include_once( APP_INC_PATH . '/pluggable.php' );
+}
+
+/**
  * Get plugins path
  *
  * Used to check for active plugins with the `is_plugin_active` function.
@@ -19,11 +34,11 @@ if ( ! function_exists( 'is_plugin_active' ) ) {
 
 	// Compatibility with ClassicPress and WordPress.
 	if ( file_exists( ABSPATH . 'wp-admin/includes/plugin.php' ) ) {
-		include( ABSPATH . 'wp-admin/includes/plugin.php' );
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
 	// Compatibility with the antibrand system.
 	} elseif ( defined( 'APP_INC_PATH' ) && file_exists( APP_INC_PATH . '/backend/plugin.php' ) ) {
-		include( APP_INC_PATH . '/backend/plugin.php' );
+		include_once( APP_INC_PATH . '/backend/plugin.php' );
 	}
 }
 
