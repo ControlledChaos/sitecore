37a02793afa6a961a0de82b3ffc4bfe7a805ceb8
ControlledChaos <greg@ccdzine.com>
2021-05-12 20:16:54 -0700

    Experimenting with settings pages.
    Forgot exactly where I was going with this.

--- includes/classes/settings/class-settings.php
+++ includes/classes/settings/class-settings.php
@@ -9,9 +9,12 @@
  */
 
 namespace SiteCore\Classes\Settings;
+
+// Alias namespaces.
 use
-SiteCore\Classes as Classes,
-SiteCore\Classes\Admin as Admin;
+SiteCore\Classes        as Classes,
+SiteCore\Classes\Admin  as Admin,
+SiteCore\Classes\Vendor as Vendor;
 
 // Restrict direct access.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -32,10 +35,36 @@ class Settings {
 	 * Setting data
 	 *
 	 * @since  1.0.0
-	 * @access private
+	 * @access protected
 	 * @var    array
 	 */
-	private $setting = [];
+	protected $setting = [];
+
+	/**
+	 * Settings page class
+	 *
+	 * The fully qualified page class with namespace.
+	 *
+	 * @example 'SiteCore\Classes\Admin\Sample_Settings_Page'
+	 *
+	 * @since  1.0.0
+	 * @access protected
+	 * @var    string Namespace & class.
+	 */
+	protected $api_page_class = 'SiteCore\Classes\Admin\Add_Settings_Page';
+
+	/**
+	 * ACF settings page class
+	 *
+	 * The fully qualified page class with namespace.
+	 *
+	 * @example 'SiteCore\Classes\Vendor\Sample_ACF_Options'
+	 *
+	 * @since  1.0.0
+	 * @access protected
+	 * @var    string Namespace & class.
+	 */
+	protected $acf_page_class = 'SiteCore\Classes\Vendor\Sample_ACF_Options';
 
 	/**
 	 * Constructor method
@@ -46,7 +75,45 @@ class Settings {
 	 * @access public
 	 * @return self
 	 */
-	public function __construct() {}
+	public function __construct() {
+
+		// Settings page.
+		$this->settings_page();
+	}
+
+	/**
+	 * Settings page
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return mixed Returns a settings page object if available,
+	 *               returns null otherwise.
+	 */
+	public function settings_page() {
+
+		// First check if ACF Pro is active.
+		if ( class_exists( 'acf_pro' ) ) {
+			$acf_active = true;
+		} else {
+			$acf_active = false;
+		}
+
+		// Native & ACF page classes.
+		$api_page = $this->api_page_class;
+		$acf_page = $this->acf_page_class;
+
+		// Run ACF settings page class if available.
+		if ( $acf_active && class_exists( $acf_page ) ) {
+			return new $acf_page;
+
+		// Run native settings page class if available.
+		} elseif ( class_exists( $api_page ) ) {
+			return new $api_page;
+		}
+
+		// Default to null.
+		return null;
+	}
 
 	/**
 	 * Settings
@@ -58,13 +125,19 @@ class Settings {
 	public function settings() {}
 
 	/**
-	 * Settings section
+	 * Existing settings section
 	 *
 	 * @since  1.0.0
-	 * @access public
+	 * @access protected
 	 * @return void
 	 */
-	public function section() {}
+	protected function existing_section() {
+
+		// Stop here if the class settings are on their own page.
+		if ( is_null( $this->settings_page() ) ) {
+			return;
+		}
+	}
 
 	/**
 	 * Setting
@@ -122,12 +195,12 @@ function get_settings() {
 	 * This includes main directory (`/`) and any
 	 * subdirectories (`* /`).
 	 */
-	$dir_file = SCP_PATH .  'includes/settings' . "{/,/*/}" . 'settings-*.php';
+	$dir_file = SCP_PATH . 'includes/settings' . "{/,/*/}" . 'settings-*.php';
 
 	// Include each file matching the path patterns.
 	foreach ( glob( $dir_file, GLOB_BRACE ) as $settings_file ) {
 		if ( is_file( $settings_file ) && is_readable( $settings_file ) ) {
-			require $settings_file;
+			include $settings_file;
 		}
 	}
 }
