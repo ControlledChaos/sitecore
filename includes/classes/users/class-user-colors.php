a0ad1cb4653f3180155d2d9fc7f2a0895c02588b
ControlledChaos <greg@ccdzine.com>
2021-10-22 08:13:37 -0700

    Correct text domain.

--- includes/classes/users/class-user-colors.php
+++ includes/classes/users/class-user-colors.php
@@ -51,7 +51,7 @@ class User_Colors {
 
 		// Rename "Default" as "Fresh".
 		if ( 'fresh' == $scheme ) {
-			return __( 'Fresh', 'dashboard-summary' );
+			return __( 'Fresh', 'sitecore' );
 		}
 
 		// The name if not the default scheme.
