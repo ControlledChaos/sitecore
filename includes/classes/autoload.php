cd61444837ad3a0fec754a8dc896cd7086538dc5
ControlledChaos <greg@ccdzine.com>
2023-03-30 18:06:53 -0700

    Autoload demo function.

--- includes/classes/autoload.php
+++ includes/classes/autoload.php
@@ -4,6 +4,8 @@
  *
  * The autoloaders register plugin classes for later use.
  *
+ * @see Demo function at end of this file.
+ *
  * @package    Site_Core
  * @subpackage Includes
  * @category   Classes
@@ -279,6 +281,52 @@ function widgets() {
 		ns( 'Widgets', 'Add_Widget' )    => f( 'widgets', 'add-widget.php' ),
 		ns( 'Widgets', 'Sample_Widget' ) => f( 'widgets', 'sample-widget.php' )
 	];
+	spl_autoload_register(
+		function ( string $class ) use ( $classes ) {
+			if ( isset( $classes[ $class ] ) ) {
+				require $classes[ $class ];
+			}
+		}
+	);
+}
+
+// Stop here for demo.
+return;
+
+/**
+ * Autoload demo
+ *
+ * The namespace and file path function are not
+ * required in the array of classes to load.
+ *
+ * In this demo function, various combinations
+ * are used in the array.
+ *
+ * @since  1.0.0
+ * @return void
+ */
+function demo() {
+
+	/**
+	 * All key => value examples would work together in
+	 * the array if these files actually existed.
+	 */
+	$classes = [
+
+		// Both functions used.
+		ns( 'Demo', 'Demo_One' ) => f( 'demo', 'demo-one.php' ),
+
+		// Full namespace & class name, path function.
+		'SiteCore\Classes\Demo\Demo_Two' => f( 'demo', 'demo-two.php' ),
+
+		// Namespace function, full path.
+		ns( 'Demo', 'Demo_Three' ) => SCP_PATH . 'includes/classes/demo/class-demo-three.php',
+
+		// Fully custom.
+		'SiteCore\Custom\Namespace\Demo_Four' => SCP_PATH . 'includes/custom/directory/class-demo-four.php'
+	];
+
+	// Autoload when in use.
 	spl_autoload_register(
 		function ( string $class ) use ( $classes ) {
 			if ( isset( $classes[ $class ] ) ) {
