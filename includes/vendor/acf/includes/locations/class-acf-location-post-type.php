1e3459caacb7c7582e460e1acc1550802271c191
ControlledChaos <greg@ccdzine.com>
2024-11-07 09:24:17 -0800

    Check for array key.

--- includes/vendor/acf/includes/locations/class-acf-location-post-type.php
+++ includes/vendor/acf/includes/locations/class-acf-location-post-type.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 if( ! class_exists('ACF_Location_Post_Type') ) :
 
 class ACF_Location_Post_Type extends ACF_Location {
-	
+
 	/**
 	 * Initializes props.
 	 *
@@ -21,7 +21,7 @@ class ACF_Location_Post_Type extends ACF_Location {
 		$this->category = 'post';
     	$this->object_type = 'post';
 	}
-	
+
 	/**
 	 * Matches the provided rule against the screen args returning a bool result.
 	 *
@@ -34,7 +34,7 @@ class ACF_Location_Post_Type extends ACF_Location {
 	 * @return	bool
 	 */
 	public function match( $rule, $screen, $field_group ) {
-		
+
 		// Check screen args.
 		if( isset($screen['post_type']) ) {
 			$post_type = $screen['post_type'];
@@ -43,11 +43,11 @@ class ACF_Location_Post_Type extends ACF_Location {
 		} else {
 			return false;
 		}
-		
+
 		// Compare rule against $post_type.
 		return $this->compare_to_rule( $post_type, $rule );
 	}
-	
+
 	/**
 	 * Returns an array of possible values for this rule type.
 	 *
@@ -58,17 +58,17 @@ class ACF_Location_Post_Type extends ACF_Location {
 	 * @return	array
 	 */
 	public function get_values( $rule ) {
-		
+
 		// Get post types.
 		$post_types = acf_get_post_types(array(
-			'show_ui'	=> 1, 
+			'show_ui'	=> 1,
 			'exclude'	=> array( 'attachment' )
 		));
-		
+
 		// Return array of [type => label].
 		return acf_get_pretty_post_types( $post_types );
 	}
-	
+
 	/**
 	 * Returns the object_subtype connected to this location.
 	 *
@@ -79,12 +79,11 @@ class ACF_Location_Post_Type extends ACF_Location {
 	 * @return	string|array
 	 */
 	public function get_object_subtype( $rule ) {
-		if( $rule['operator'] === '==' ) {
+		if ( array_key_exists( 'value', $rule ) && '==' === $rule['operator'] ) {
 			return $rule['value'];
 		}
 		return '';
 	}
-	
 }
 
 // initialize
